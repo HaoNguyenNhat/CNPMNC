5c96456d7c3811097e3bc92a8e61091546b2a267
HaoNguyenNhat <haomache@gmail.com>
2023-04-26 20:46:42 +0700

    a

--- CNPMNC-Source Code/backend/src/main/java/com/devcamp/springjwt/repository/ProductLineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devcamp.springjwt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devcamp.springjwt.models.ProductLine;
-
-public interface ProductLineRepository extends JpaRepository<ProductLine, Integer> {
-    
-}
