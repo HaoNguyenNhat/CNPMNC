0b44accb88e257ac910e84058a02e0759d642a5f
HaoNguyenNhat <haomache@gmail.com>
2023-05-22 20:33:36 +0700

    haizz

--- backend/src/main/java/com/devcamp/springjwt/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devcamp.springjwt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devcamp.springjwt.models.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    
-}
